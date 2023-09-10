0b428bede36282789db55cceac18be4377234911
Narasimha Raja <snraja@users.noreply.github.com>
2023-09-10 23:53:16 +0530

    Added support for JSON Data

---
+++ J2EE/hello-world-jax-rs/src/main/java/com/snraja/j2ee/jaxrs/AreaModel.java
@@ -0,0 +1,13 @@
+package com.snraja.j2ee.jaxrs;
+
+public class AreaModel {
+	public String name;
+	public String area;
+	
+	public AreaModel(String name, String area) {
+		super();
+		this.name = name;
+		this.area = area;
+	}
+	
+}
--- J2EE/hello-world-jax-rs/src/main/java/com/snraja/j2ee/jaxrs/AreaService.java
+++ J2EE/hello-world-jax-rs/src/main/java/com/snraja/j2ee/jaxrs/AreaService.java
@@ -1,5 +1,6 @@
 package com.snraja.j2ee.jaxrs;
 
+import java.util.*;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
@@ -7,9 +8,32 @@ import javax.ws.rs.core.MediaType;
 public class AreaService {
 
 	@GET
-	@Path("/cities")
+	@Path("/cities-json")
 	@Produces(MediaType.APPLICATION_JSON)
-	public String getArea() {
-		return "{ Mumbai : 603.4 km^2 \n Bangalore : 741 km^2 \n Chennai : 426 km^2 } ";
+	public List<AreaModel> getArea() {
+		
+		return this.getCityAreasModel();
+	}
+	
+	@GET
+	@Path("/cities-text")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getAreaInText() {
+		return "Delhi, Bengaluru, Visakhapatnam, Hyderabad, Lucknow, Indore, Ahmedabad, Pune, Bhopal, Surat";
+	}
+	
+	private List<AreaModel> getCityAreasModel() {
+		List<AreaModel> list = new ArrayList<AreaModel>();
+		list.add(new AreaModel("Delhi", "1398 km^2"));
+		list.add(new AreaModel("Bengaluru", "741 km^2"));
+		list.add(new AreaModel("Visakhapatnam", "681 km^2"));
+		list.add(new AreaModel("Hyderabad", "650 km^2"));
+		list.add(new AreaModel("Lucknow", "631 km^2"));
+		list.add(new AreaModel("Indore", "564 km^2"));
+		list.add(new AreaModel("Ahmedabad", "505 km^2"));
+		list.add(new AreaModel("Pune", "485 km^2"));
+		list.add(new AreaModel("Bhopal", "463 km^2"));
+		list.add(new AreaModel("Surat", "461 km^2"));
+		return list;
 	}
 }
